fc6705236598a93157452c5e66ecbca18788840e
Apurva Gawade <154744554+ApurvaCodes@users.noreply.github.com>
2024-09-21 14:38:44 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -25,16 +25,24 @@ $(document).ready(function () {
     progressObserver.observe(this);
   });
 
-  // Resume download function
-  $(".resume-button").on("click", function (e) {
-    e.preventDefault();
-    const link = document.createElement('a');
-    link.href = 'RESUME.pdf';  // Replace with the actual path to your PDF
-    link.download = 'Apurva_Gawade_Resume.pdf';  // The file name for the downloaded file
-    link.target = '_blank';  // Open in a new tab as well as download
-    document.body.appendChild(link);  // Append link to document
-    link.click();  // Programmatically click the link
-    document.body.removeChild(link);  // Clean up the DOM
-  });
+  
+    // Resume download function
+    $(".resume-link").on("click", function (e) {
+        e.preventDefault();
+        const resumeUrl = 'RESUME.pdf';  // Replace with the actual path to your PDF
+        
+        // Open the PDF in a new tab
+        window.open(resumeUrl, '_blank');
+
+        // Create a temporary link for downloading
+        const link = document.createElement('a');
+        link.href = resumeUrl;
+        link.download = 'Apurva_Gawade_Resume.pdf';  // The file name for the downloaded file
+        document.body.appendChild(link);  // Append link to document
+        link.click();  // Programmatically click the link
+        document.body.removeChild(link);  // Clean up the DOM
+    });
 });
+
+
 
